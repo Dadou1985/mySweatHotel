d528ba842a226bd1a0e11dcb4f880b3ca9e21435
dadou1985 <davidsimba1985@yahoo.com>
2021-03-20 02:42:43 +0100

    (firebase.js/register.js) connect firebase + implement register function: done

---
+++ .history/firebase_20210320005229.js
@@ -0,0 +1,26 @@
+import * as firebase from 'firebase'
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/functions";
+import "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCDGf-DWOM8z-I4nC2jg0PxsfLKQ_GE7o0",
+    authDomain: "notel-765b1.firebaseapp.com",
+    databaseURL: "https://notel-765b1.firebaseio.com",
+    projectId: "notel-765b1",
+    storageBucket: "notel-765b1.appspot.com",
+    messagingSenderId: "746435372425",
+    appId: "1:746435372425:web:75bd50c1e1494d3304ed58",
+    measurementId: "G-7D7WHNS6M7"
+  };
+
+  let app;
+
+  if(firebase.apps.length === 0) {
+      app =   firebase.initializeApp(firebaseConfig);
+  } else{
+      app = firebase.app();
+  }
+ 
+const db = app.firestore()
---
+++ .history/src/screens/Register_20210320003820.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Button, Input, Image } from 'react-native-elements';
+import { StatusBar } from 'expo-status-bar';
+import { MaterialIcons } from '@expo/vector-icons'; 
+
+
+const Register = ({ navigation }) => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [name, setName] = useState("")
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setEmail("")
+        setPassword('')
+        setConfirmPassword('')
+        setName('')
+            if(password === confirmPassword){
+               return firebase.register({username: name, email: email, password: password})
+                .catch(error=>{
+                    if(error.message){
+                        alert("Inscription invalide")
+                    }else{}
+                })
+            }else{
+                setEmail("")
+                setPassword('')
+                setConfirmPassword('')
+                setName('')
+                return alert("Désolé, confirmation de mot de passe incorrecte !")
+            }
+      }
+
+    return (
+        <KeyboardAvoidingView style={styles.container}>
+            <StatusBar style="light" />
+            <View style={styles.containerText}>
+                <Text style={styles.text}>Créer un compte</Text>
+            </View>    
+                <View style={styles.inputContainer}> 
+                <Input placeholder="Nom" autofocus type="text" value={name} 
+                onChange={(text) => setName(text)} />
+                <Input placeholder="Email" type="email" value={email} 
+                onChange={(text) => setEmail(text)} />
+                <Input placeholder="Mot de passe" secureTextEntry type="password" value={password} 
+                onChange={(text) => setPassword(text)} />
+                <Input placeholder="Confirmation du mot de passe" secureTextEntry type="confirmPassword" value={confirmPassword} 
+                onChange={(text) => setConfirmPassword(text)}
+                onSubmitEditing={handleSubmit} />
+            </View>
+            <View style={{marginBottom: 15}}>
+                <TouchableOpacity style={{flexDirection: "row", width: 300, alignItems: "center", justifyContent: "center"}}>
+                <MaterialIcons name="add-a-photo" size={24} color="grey" />                    
+                <Text style={{fontSize: 20, color: "grey", marginLeft: 10}}>Ajouter une image</Text>
+                </TouchableOpacity>
+            </View>
+            <Button onPress={() => navigation.navigate('Connexion')} containerStyle={styles.button} title="Connexion" type="clear" />
+            <Button containerStyle={styles.button} title="Créer un compte" onPress={handleSubmit} />
+        </KeyboardAvoidingView>
+    )
+}
+
+export default Register
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10
+    },
+    containerText: {
+        marginBottom: 30
+    },
+    text: {
+        fontSize: 30,
+        textAlign: "center"
+    },
+    inputContainer: {
+        width: 300
+    },
+    button: {
+        width: 200,
+        marginTop: 10
+    }
+})
